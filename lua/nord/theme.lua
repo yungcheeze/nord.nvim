f220e7625129d2fae98dc07905ceff61803dd625
Ucizi Mafeni <ucizim@gmail.com>
2022-01-13 16:13:15 +0000

    preserve text colour in diff

--- lua/nord/theme.lua
+++ lua/nord/theme.lua
@@ -85,10 +85,10 @@ theme.loadEditor = function ()
     Cursor =           { fg = nord.nord4_gui, bg = nord.none, style = 'reverse' }, -- the character under the cursor
     CursorIM =         { fg = nord.nord5_gui, bg = nord.none, style = 'reverse' }, -- like Cursor, but used when in IME mode
     Directory =        { fg = nord.nord7_gui, bg = nord.none }, -- directory names (and other special names in listings)
-    DiffAdd =          { fg = nord.nord14_gui, bg = nord.none, style = 'reverse' }, -- diff mode: Added line
-    DiffChange =       { fg = nord.nord13_gui, bg = nord.none, style = 'reverse' }, --  diff mode: Changed line
-    DiffDelete =       { fg = nord.nord11_gui, bg = nord.none, style = 'reverse' }, -- diff mode: Deleted line
-    DiffText =         { fg = nord.nord15_gui, bg = nord.none, style = 'reverse' }, -- diff mode: Changed text within a changed line
+    DiffAdd =          { bg = nord.nord14_gui}, -- diff mode: Added line
+    DiffChange =       { bg = nord.nord13_gui}, --  diff mode: Changed line
+    DiffDelete =       { bg = nord.nord11_gui}, -- diff mode: Deleted line
+    DiffText =         { bg = nord.nord15_gui}, -- diff mode: Changed text within a changed line
     EndOfBuffer =      { fg = nord.nord1_gui },
     ErrorMsg =         { fg = nord.none },
     Folded =           { fg = nord.nord3_gui_bright, bg = nord.none, style = 'italic' },
